6d695e7ebee3e518d1a173dfd4c80ec2def9bf12
Davian Affognon <affogdav000@warren.k12.in.us>
2024-09-19 11:56:54 -0400

    yeah

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,2 +1,35 @@
-public class GreatestCommonDivisor {
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDenominator(25, 15));
+        System.out.println(getGreatestCommonDenominator(12, 30));
+        System.out.println(getGreatestCommonDenominator(9, 18));
+        System.out.println(getGreatestCommonDenominator(81, 153));
+
+    }
+
+    private static int getGreatestCommonDenominator(int first, int second) {
+        if(first < 10 || second < 10){
+            return -1;
+        }
+        int gcd = 1;
+        int smallest = 0;
+
+
+        if(first <= second) {
+            smallest = first;
+        }else{
+            smallest = second;
+        }
+
+        for(int i = 1; i <= smallest; i++){
+            if(first % i == 0 && second % i == 0){
+                gcd = i;
+            }
+        }
+
+        return gcd;
+
+    }
+
 }
